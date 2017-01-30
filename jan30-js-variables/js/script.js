f3adb98f4e7a7ea0c74f0eba4186a2f26e676dbf
David Rodriguez <rodriguezdavid99@gmail.com>
2017-01-30 15:50:37 -0800

    jan30-js-variables / Learning the basics of JavaScript

---
+++ jan30-js-variables/js/script.js
@@ -0,0 +1,21 @@
+// alert('this is an alert');
+
+var myNumberVar = 7; // number object
+var myFloatVar = 5.2; // float object
+var myStringVar = "David Rodriguez"; // string object
+var myBooleanVar = true; // boolean object
+var myArrayVar = ['one', 'stingray', 'banana', 8, true]; // array object
+var myObjectVar = { // object object
+    make: 'Honda',
+    model: 'Car',
+    color: 'red',
+    year: 2014
+};
+
+
+console.log(typeof myNumberVar);
+console.log(typeof myFloatVar);
+console.log(typeof myStringVar);
+console.log(typeof myBooleanVar);
+console.log(typeof myArrayVar);
+console.log(myObjectVar.model);
