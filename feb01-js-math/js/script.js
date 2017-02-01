9a4e6a34eecd19ed481e8bed6d381ab291c470ea
David Rodriguez <rodriguezdavid99@gmail.com>
2017-02-01 15:52:33 -0800

    feb01-js-math / Using basic math in JavaScript

---
+++ feb01-js-math/js/script.js
@@ -0,0 +1,55 @@
+// 6 Object Types
+
+// Object Object
+// String Object
+// Number Object
+// Float Object
+// Boolean Object
+// Array Object
+
+var myObjectVar = {
+    year: 1989,
+    make: 'honda',
+    model: 'explorer',
+    color: 'black'
+};
+
+var myStringVar = 'string';
+
+var myNumberVar = 9;
+
+var myFloatVar = 2.6;
+
+var myBooleanVar = false;
+
+var myArrayVar = ['my', 'name', 'is', 'david', true];
+
+
+
+console.log(myObjectVar);
+console.log(myStringVar);
+console.log(myNumberVar);
+console.log(myFloatVar);
+console.log(myBooleanVar);
+console.log(myArrayVar);
+
+
+console.log(typeof myArrayVar);
+console.log(typeof myNumberVar);
+console.log(myObjectVar.make);
+
+
+var firstnum = 4;
+var secondnum = 5;
+
+var addnum = (firstnum + secondnum);
+console.log(addnum);
+
+var multnum = (firstnum * secondnum);
+console.log(multnum);
+
+var divnum = (firstnum / secondnum);
+console.log(divnum);
+
+var subnum = (firstnum - secondnum);
+console.log(subnum);
