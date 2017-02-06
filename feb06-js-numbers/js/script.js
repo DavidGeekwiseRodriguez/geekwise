850f1d3c47bd957cb414537f29d2ea2f4bedf467
David Rodriguez <rodriguezdavid99@gmail.com>
2017-02-06 15:53:00 -0800

    feb06-js-numbers feb06-js-story / Learning number objects and creating a story

---
+++ feb06-js-numbers/js/script.js
@@ -0,0 +1,66 @@
+var myNumber = 5;
+var myBoolean = true;
+var myObject = {
+  make: "ravioli",
+  model: "chef boyardee",
+  year: 1965
+};
+var myFloat = 5.5;
+var myString = "blutel is the best";
+var myArray = ["blutel", "is", "the", "best", "1337"];
+
+// console.log(myNumber);
+// console.log(myBoolean);
+// console.log(myObject);
+// console.log(myFloat);
+// console.log(myString);
+// console.log(myArray);
+//
+// console.log(myBool);
+// console.log(myString.indexOf('i', 3));
+// console.log(myArray[4]);
+// console.log(myObject.ravioli);
+
+/*
+  Number Object
+*/
+
+
+var numOne = 2;
+var numTwo = new Number(5);
+// console.log(numOne);
+// console.log(numTwo);
+
+var numThree = '3';
+var numFour = 5.2;
+
+// console.log(parseInt(numThree));
+// console.log(parseFloat(numFour));
+
+var numFive = 'David';
+// console.log(parseInt(numFive));
+// console.log(parseFloat(numFive));
+
+if(isNaN(numFive)){
+    // alert('rip lmao');
+}
+
+//parseInt
+//parseFloat
+//isNaN
+
+// alert('Wha?');
+// // var userLang = confirm('Do you speak it?')
+// var formuoli = prompt('What is your favorite flavor of ravioli?');
+// console.log(formuoli);
+
+// console.log(userLang);
+
+// var firstName = prompt('What is your first name?');
+// var mainSmash = prompt('Who do you main in Smash?');
+// var topPlayers = prompt('Which top players do you know?');
+//
+// console.log( firstName + " " + mainSmash + " " + topPlayers );
+
+// var phoneNum = prompt('give me your numba gurl')
+// console.log(parseInt(phoneNum));
---
+++ feb06-js-story/js/script.js
@@ -0,0 +1,32 @@
+// All 3 alerts (alert, prompt, confirm)
+// Utilize 4 objects. (String, Number, FLoat, Boolean)
+// Minimum of 10 variable saves
+// Minimum of 5 confirms
+// Use 1 if statement
+// Use 2 mathematical operations (+, -, *, /)
+// Assumed usage of concatenation
+/*
+  if( isNaN(var) ){
+      alert('its not a number');
+  }else{
+      alert('it is a number');
+  }
+*/
+
+
+
+alert('As you slowly open your eyes, the sun blinds you as you regain vision.');
+
+alert('The blurred environment gradually clears up as your confusion begins to set in.');
+
+alert("The grass appears to be your height! You panic as you realize what situation you're in.");
+
+alert('"Oh my god," you think to yourself.');
+
+alert("You're a garden gnome.");
+
+alert('You try to scream, but you are unable to. There are many questions you have about yourself.')
+
+var gnoName = prompt('You try to remember your name. What is your name?');
+var yeahNo = alert('Yeah, no, ' + gnoName + " isn't going to work.")
+alert('Your name is now Keemstar.')
